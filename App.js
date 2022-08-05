46cf6b1333a7db50dbedba53d1b00ebbf4c56f77
furknhelim <furkanhelim.kara28@gmail.com>
2022-08-05 13:11:31 +0300

    asd

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
 import { View } from "react-native";
 import MainMenu from "./src/pages/MainMenu";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  
+
 
   return (
     <NavigationContainer>
