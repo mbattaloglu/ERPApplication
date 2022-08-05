31144361d95ff9ccb23e3eafc1eaacd308f3e267
mbattaloglu <mmbattaloglu@gmail.com>
2022-08-05 11:57:12 +0300

    1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
-
+//Yorum
 const App = () => {
   return (
     <NavigationContainer>
