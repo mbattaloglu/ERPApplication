2924562c2549c7a76c61d6479b5253ddf19334d3
furknhelim <furkanhelim.kara28@gmail.com>
2022-08-05 13:08:37 +0300

    ada

--- App.js
+++ App.js
@@ -2,27 +2,19 @@ import React, { useEffect } from "react";
 import { View } from "react-native";
 import MainMenu from "./src/pages/MainMenu";
 
+
 const App = () => {
-  /*useEffect(() => {
-    fetch('http://193.53.103.178:5312/api/Authentication', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        username: 'osmanportal',
-        password: '123',
-        // expiresInMins: 60, // optional
-      })
-    })
-      .then(res => res.json())
-      .then(res => User.token = res)
-      .then(console.log(User.token))
-      .catch((e) => alert(e))
-  }, [])*/
+  
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <MainMenu />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={() => ({
+          headerShown: false,
+        })}>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
